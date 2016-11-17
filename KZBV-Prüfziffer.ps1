f615c47f2d3f74f13191edc41b9ff7952cb56b65
Delapro <delapro@easysoftware.de>
2016-11-17 23:01:42 +0100

    erste Erfassung

---
+++ KZBV-Prüfziffer.ps1
@@ -0,0 +1,109 @@
+# Powershell KZBV-Prüfzifferberechnung
+#
+# erstellt von easy - innovative software
+#
+# zur Unterstützung der VDDS-Laborschnittstelle: http://www.vdds.de/content/de/laborschnittstelle.php
+# http://www.kzbv.de/papierlose-abrechnung.98.de.html
+
+# ermittelt die zum übergebenen String passende Prüfziffer nach dem
+# Algorithmus der KZBV
+Function Invoke-KZBVPrüfzifferBerechnung {
+    [CmdletBinding()]
+    Param(
+        [String]$Auftragsnummer
+    )
+
+    $PZ_ZAHLEN = "0123456789"
+    $PZ_GROSSBUCHSTABEN = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    $PZ_KLEINBUCHSTABEN = "abcdefghijklmnopqrstuvwxyz"
+    $PZ_TRENNER = "-*"
+
+    $len = $Auftragsnummer.Length
+    $pos = 0
+    $shift = 1
+    $sum = 0
+
+    while ($len-- -gt 0) {
+        $byte = [byte]$Auftragsnummer.Substring($pos++, 1)[0]
+        $byte = $byte -band 127
+        If (($PZ_TRENNER + $PZ_ZAHLEN + $PZ_GROSSBUCHSTABEN + $PZ_KLEINBUCHSTABEN).IndexOf([char]$Byte) -eq -1) {
+            throw "Ungültiges Zeichen in Auftragsnummer"
+        } else {
+            If ($PZ_TRENNER.IndexOf([char]$byte) -ge 0) {
+                $byte = [byte]"0"[0]
+            }
+            # If ($PZ_ZAHLEN.IndexOf($byte.ToString()))
+            If ($PZ_GROSSBUCHSTABEN.IndexOf([char]$byte) -ge 0) {
+                $byte = $byte -bor 32
+            }
+            If ($PZ_KLEINBUCHSTABEN.IndexOf([char]$byte) -ge 0) {
+                $byte = $byte % 10 + [byte]"0"[0]
+            }
+        }
+
+        $byte = $byte - [byte]"0"[0]
+        $cross = $byte -shl $shift
+        If ($cross -gt 9) {
+            $cross -= 9
+        }
+        $sum += $cross
+
+        $shift = $shift -bxor 1
+
+    }
+
+    [char]([byte]"0"[0] + $sum % 10)
+}
+
+# gibt das letzte Zeichen der XML-Auftragsnummer zurück
+Function Get-KZBVPrüfziffer {
+   [CmdletBinding()]
+    Param(
+        [String]$Auftragsnummer
+    )
+
+    $Auftragsnummer.Substring($Auftragsnummer.Length -1)
+}
+
+# prüft eine XML-Auftragsnummer auf Korrektheit und gibt $true/$false zurück
+Function Test-KZBVPrüfziffer {
+   [CmdletBinding()]
+    Param(
+        [String]$Auftragsnummer
+    )
+
+    $Prüfziffer = Invoke-KZBVPrüfzifferBerechnung -Auftragsnummer $Auftragsnummer.Substring(0, $Auftragsnummer.Length -1)
+    If ($Prüfziffer -eq (Get-KZBVPrüfziffer -Auftragsnummer $Auftragsnummer)) {
+        $true
+    } else {
+        $false
+    }
+}
+
+# testet alle Dateinamen mit Bindestrich und XML-Endung
+Function Test-Directory {
+   [CmdletBinding()]
+    Param(
+        [String]$Directory
+    )
+
+    $f = Get-Item -Path "$($Directory)\*.xml"
+    $f | % {
+                If ($_.Name -match "-") {
+                    $n = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+                    try {
+                        $p = Invoke-KZBVPrüfzifferBerechnung -Auftragsnummer $n.Substring(0, $n.Length -1)
+                        $v = Test-KZBVPrüfziffer -Auftragsnummer $n
+                    } catch {
+                        $p = ""
+                        $v = $false
+                    }
+
+                    [PSCustomObject]@{"Auftragsnummer"=$n
+                                      "Prüfziffer"=$p
+                                      "Gültig"=$v
+                                     }
+                }
+            }
+}
+
